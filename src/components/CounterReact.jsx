e9e03c38fc17a607a2bd10ddaa37e7654c8753ec
Guillermo Hernandez <hernandez17.guillermo@gmail.com>
2023-01-29 19:15:25 -0300

    feat: commit temporally

--- src/components/CounterReact.jsx
+++ src/components/CounterReact.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import CardCharacter from './CardCharacter.jsx';
 
 const Counter = () => {
-  const [counter, setCounter] = useState(0);
+  const [characters, setCharacters] = useState({});
+
+  useEffect(() => {
+    async function getCharacters() {
+      const response = (await axios.get('https://rickandmortyapi.com/api/character')).data.results;
+      setCharacters(response);
+    }
+
+    getCharacters();
+  }, []);
 
   return (
-    <div>
-      <button onClick={() => setCounter(counter => counter + 1)}>+</button>
-      <h1>{counter}</h1>
-    </div>
+    <>
+      <div className="bg-slate-800 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 place-items-center p-10">
+        {characters.length && characters.map(({ name, image }) => <CardCharacter name={name} image={image} />)}
+      </div>
+    </>
   );
 };
 
